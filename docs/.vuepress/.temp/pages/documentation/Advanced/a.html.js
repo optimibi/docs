a51b966691ac244902f8a6bea24a46a9017b5e21
meishenlieshou <meishenlieshou@gmail.com>
2025-02-11 13:37:53 +0800

    folder commit

---
+++ docs/.vuepress/.temp/pages/documentation/Advanced/a.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/Administrator/Desktop/workspace/optimibi/docs/docs/.vuepress/.temp/pages/documentation/Advanced/a.html.vue"
+const data = JSON.parse("{\"path\":\"/documentation/Advanced/a.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0,\"words\":0},\"filePathRelative\":\"documentation/Advanced/a.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ docs/documentation/Tools/a.md
@@ -0,0 +1,5 @@
+---
+title: a
+createTime: 2025/02/11 13:37:07
+permalink: /documentation/Advanced/a/
+---
