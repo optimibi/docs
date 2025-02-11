1356f1043822cfba8694fc71121d608b4938f17b
meishenlieshou <meishenlieshou@gmail.com>
2025-02-11 13:47:05 +0800

    commit

--- docs/.vuepress/.temp/pages/documentation/Update/9GZxGdmid/index.html.js
+++ docs/.vuepress/.temp/pages/documentation/Update/9GZxGdmid/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/Administrator/Desktop/workspace/optimibi/docs/docs/.vuepress/.temp/pages/documentation/Update/9GZxGdmid/index.html.vue"
-const data = JSON.parse("{\"path\":\"/documentation/Update/9GZxGdmid/\",\"title\":\"2024-7 v6.06\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"2024-7 v6.06\",\"createTime\":\"2025/02/11 08:49:04\",\"permalink\":\"/documentation/Update/9GZxGdmid/\",\"author\":\"Jack\",\"tags\":[\"Update\"],\"description\":\"description\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.56,\"words\":167},\"filePathRelative\":\"documentation/Update/2024-7 v6.06.md\"}")
+const data = JSON.parse("{\"path\":\"/documentation/Update/9GZxGdmid/\",\"title\":\"2024-7 v6.06\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"2024-7 v6.06\",\"createTime\":\"2025/02/11 08:49:04\",\"permalink\":\"/documentation/Update/9GZxGdmid/\",\"author\":\"Optimibibi\",\"lastUpdated\":false,\"contributors\":false,\"tags\":[\"Update\"],\"description\":\"description\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.57,\"words\":171},\"filePathRelative\":\"documentation/Update/2024-7 v6.06.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
