f84914dd4d6ca73ad7fec66938eddd7569e73b57
meishenlieshou <meishenlieshou@gmail.com>
2025-02-11 13:42:23 +0800

    Commit

---
+++ docs/.vuepress/.temp/pages/documentation/Console/a/index.html.js
@@ -0,0 +1,16 @@
+import comp from "C:/Users/Administrator/Desktop/workspace/optimibi/docs/docs/.vuepress/.temp/pages/documentation/Console/a/index.html.vue"
+const data = JSON.parse("{\"path\":\"/documentation/Console/a/\",\"title\":\"a\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"a\",\"createTime\":\"2025/02/11 13:37:07\",\"permalink\":\"/documentation/Console/a/\",\"gitInclude\":[]},\"headers\":[],\"readingTime\":{\"minutes\":0.03,\"words\":9},\"filePathRelative\":\"documentation/Console/a.md\"}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
--- docs/documentation/System/a.md
+++ docs/documentation/System/a.md
@@ -1,5 +1,5 @@
 ---
 title: a
 createTime: 2025/02/11 13:37:07
-permalink: /documentation/Advanced/a/
+permalink: /documentation/System/a/
 ---
