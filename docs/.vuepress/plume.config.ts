197b5caba8ddecc1d19ca9b1f9f0678f6d3bf08f
meishenlieshou <meishenlieshou@gmail.com>
2025-02-11 11:46:23 +0800

    404 commit

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -15,7 +15,13 @@ export default defineThemeConfig({
 	// aside: true, // 页内侧边栏， 默认显示在右侧
 	// outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
-	notFound: {},
+	notFound: {
+		code: " ",
+		title: " ",
+		quote: " ",
+		linkLabel: " ",
+		linkText: " ",
+	},
 
 	/**
 	 * 文章版权信息
