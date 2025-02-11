e44127da8cfdcd93977e23b567f0ff2ca01f78bb
meishenlieshou <meishenlieshou@gmail.com>
2025-02-11 11:42:04 +0800

    404config

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -15,6 +15,8 @@ export default defineThemeConfig({
 	// aside: true, // 页内侧边栏， 默认显示在右侧
 	// outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
+	notFound: {},
+
 	/**
 	 * 文章版权信息
 	 * @see https://theme-plume.vuejs.press/guide/features/copyright/
