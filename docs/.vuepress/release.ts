ac08a9988a58d7ea21efeb32c72998cf15381b63
meishenlieshou <meishenlieshou@gmail.com>
2025-02-27 13:58:58 +0800

    Update release.ts

--- docs/.vuepress/release.ts
+++ docs/.vuepress/release.ts
@@ -3,7 +3,16 @@ import { defineNoteConfig } from "vuepress-theme-plume";
 export const releaseNote = defineNoteConfig({
 	dir: "release",
 	link: "/release",
-	sidebar: ["100 v6.06 Release Notes.md", "test.md"],
+	sidebar: [
+		{
+			text: "Release Notes",
+			link: "/release/100 v6.06 Release Notes.md",
+		},
+		{
+			text: "v6.05",
+			link: "/release/test.md",
+		},
+	],
 });
 
 export const releaseNoteZH = defineNoteConfig({
